65f91b482c25784791f7bcbd6cca6f7072bb1310
Mikita Slabysh <slabysh2015@gmail.com>
2024-10-23 00:38:33 -0400

    Init Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerController : NetworkBehaviour {
+  public float speed;
+
+  private ClientNetworkTransform _transform;
+
+  private void Start() {
+    _transform = GetComponent<ClientNetworkTransform>();
+  }
+
+  private void Update() {
+    if(!IsOwner) return;
+    var movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    _transform.transform.position += movement * speed * Time.deltaTime;
+  }
+}
+
